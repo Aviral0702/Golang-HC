db8f862096d3e8597c1b89dfefa5da0d6bb6c66a
Aviral0702 <aviral.asthana0704@gmail.com>
2024-07-17 15:01:00 +0530

    Add code for handling web URLs and printing another URL in main.go

--- Basics/Handling web url/main.go
+++ Basics/Handling web url/main.go
@@ -14,11 +14,19 @@ func main() {
 	fmt.Println("Result Scheme is: ", result.Scheme)
 	fmt.Println("Result Host is: ", result.Host)
 	fmt.Println("Result Path is: ", result.Path)
-
+	fmt.Println("Result rawpath is : ", result.RawPath)
 	qparams := result.Query()
 	fmt.Printf("The type of query params is: %T\n", qparams)
 
 	for _, values := range qparams {
 		fmt.Println("Param is: ", values)
 	}
+
+	partsOfUrl := &url.URL{
+		Scheme: "https",
+		Host:   "api.github.com",
+		Path:   "/users/Aviral0702",
+	}
+	anotherurl := partsOfUrl.String()
+	fmt.Println(anotherurl)
 }
