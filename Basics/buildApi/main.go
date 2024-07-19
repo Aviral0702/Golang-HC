1b2902641a8ca0b2f859bd110e831d1358e04369
Aviral0702 <aviral.asthana0704@gmail.com>
2024-07-19 22:42:55 +0530

    feat: Add Go module and dependencies for build API

---
+++ Basics/buildApi/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Course struct {
+	CourseId    string `json:"courseid"`
+	CourseName  string `json:"coursename"`
+	CoursePrice string `json:"price"`
+	Author      Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+var mycourses []Course
+
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to the home page</h1>"))
+}
