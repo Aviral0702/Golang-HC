8f764fecb4dd3dd1d49458a7fbe65eb291429b79
Aviral0702 <aviral.asthana0704@gmail.com>
2024-07-22 22:14:30 +0530

    feat: Add insertOneMovie function to insert a movie into the database

--- Basics/mongoApi/main.go
+++ Basics/mongoApi/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Aviral0702/mongoApi/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var collection *mongo.Collection
+
 func main() {
 
+}
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one movie to the database with id: ", inserted.InsertedID)
 }
