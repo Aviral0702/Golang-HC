94b85b0f13fcb61d3ab2206e9eac69c394e64de3
Aviral0702 <aviral.asthana0704@gmail.com>
2024-07-23 07:59:50 +0530

    feat: Add CRUD operations for movies in the Mongo API

--- Basics/mongoApi/contoller/controller.go
+++ Basics/mongoApi/contoller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/Aviral0702/mongoApi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -32,4 +35,47 @@ func init() {
 	//collection instance
 	fmt.Println("Collection instance created")
 
+}
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one movie to the database with id: ", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Updated one movie to the database with count: ", result.ModifiedCount)
+}
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got delete with delete account: ", deleteCount)
+}
+
+func deleteAllMovies() int64 {
+	const deletedResult,err := collection.DeleteMany(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Deleted all movies with count: ", deletedResult.DeletedCount,nil)
+	return deletedResult.DeletedCount
 }
--- Basics/mongoApi/main.go
+++ Basics/mongoApi/main.go
@@ -1,40 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-
-	"github.com/Aviral0702/mongoApi/model"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-var collection *mongo.Collection
-
 func main() {
 
-}
-
-func insertOneMovie(movie model.Netflix) {
-	inserted, err := collection.InsertOne(context.Background(), movie)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Inserted one movie to the database with id: ", inserted.InsertedID)
-}
-
-func updateOneMovie(movieId string) {
-	id, _ := primitive.ObjectIDFromHex(movieId)
-	filter := bson.M{"_id": id}
-	update := bson.M{"$set": bson.M{"watched": true}}
-
-	result, err := collection.UpdateOne(context.Background(), filter, update)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Updated one movie to the database with count: ", result.ModifiedCount)
 }
