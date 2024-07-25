c32d74841e8a21e08b72df3781fb843ef214e7d7
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-07-25 16:50:27 +0000

    feat: Implement concurrent HTTP requests to check status codes of multiple websites

--- Basics/goroutines/main.go
+++ Basics/goroutines/main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
-	go greeter("hello")
-	greeter("world")
+	// go greeter("hello")
+	// greeter("world")
+	websiteList := []string{
+		"https://google.com",
+		"https://fb.com",
+		"https://amazon.com",
+		"https://youtube.com",
+	}
+
+	for _, web := range websiteList {
+		time.Sleep(3 * time.Millisecond) //this doesn't work as expected I tried
+		go getStatusCode(web)
+	}
 }
 
-func greeter(msg string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(msg)
+// func greeter(msg string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(msg)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
 	}
+
+	fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 }
